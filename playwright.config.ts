8e5487b579898ce271d0c8614d0cf69bcea46bc2
dariossneto <dariosnneto@gmail.com>
2026-03-12 13:16:36 -0300

    Adding fullyparallel true

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 2 : undefined,
